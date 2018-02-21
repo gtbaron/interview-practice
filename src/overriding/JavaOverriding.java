7425a3e7b20a0d9c538402498d4c055551266df9
Mike Sanders <sanders.michael.j@gmail.com>
2018-02-21 11:23:35 -0500

    fixed comment

--- src/overriding/JavaOverriding.java
+++ src/overriding/JavaOverriding.java
@@ -6,7 +6,7 @@ public class JavaOverriding extends Parent {
         // override the printMessage method from the parent class to print a different message
         this.printMessage();
 
-        // updated this class to implement the Teapot interface, add the required method, and call it on the next line
+        // update this class to implement the Teapot interface, add the required method, and call it on the next line
     }
 
     public static void main(String[] args) {
